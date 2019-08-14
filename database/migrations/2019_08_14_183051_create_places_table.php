866f181cb2bd14cb923837c6a1bcce25ab6f76fc
dmitriyl99 <dmitriyl1899@gmail.com>
2019-08-14 23:43:37 +0500

    Add Place model

---
+++ database/migrations/2019_08_14_183051_create_places_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('description');
+            $table->string('img_url')->nullable();
+            $table->integer('location_id')->unsigned()->nullable();
+            $table->integer('area_id')->unsigned()->nullable();
+
+            $table->foreign('location_id')->on('locations')->onDelete('cascade');
+            $table->foreign('area_id')->on('areas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('places');
+    }
+}
