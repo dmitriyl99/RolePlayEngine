1df6fd6950fb16c4a1d2d57ecb84e4037c6e07d6
dmitriyl99 <dmitriyl1899@gmail.com>
2020-01-01 15:58:39 +0500

    Create layout for app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,101 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!doctype html>
+<html lang="ru">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="{{ config('app.name') }} role play project">
+    <meta name="author" content="skaydi">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta property="og:title" content="{{ config('app.name') }} role play project">
+    <meta property="og:site_name" content="{{ config('app.name') }}">
+    <meta property="og:description" content="{{ config('app.name') }} role play project">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Icons -->
+    <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicons/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('assets/img/favicons/favicon-192x192.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/favicons/apple-touch-icon-180x180.png') }}">
+    <!-- END Icons -->
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Stylesheets -->
+    <!-- Codebase framework -->
+    <link rel="stylesheet" id="css-main" href="{{ asset('assets/css/codebase.min.css') }}">
+    <link rel="stylesheet" id="css-theme" href="{{ asset('assets/css/themes/pulse.min.css') }}">
+    @yield('css')
+    <!-- END Stylesheets -->
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>@yield('title') | {{ config('app.name') }}</title>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
+    <div id="page-container" class="side-scroll page-header-modern page-header-inverse page-header-fixed">
+        <header id="page-header">
+            <div class="content-header">
+                <div class="content-header-section">
+                    <div class="content-header-item">
+                        <a href="{{ route('home') }}" class="link-effect font-w600">
+                            <span class="text-dual-primary-dark">S.T.A.L.K.E.R.</span>
+                            <span class="text-primary">Role play</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="content-header-section">
+                    <ul class="nav-main-header">
+                        <li><a href="#">Южные земли</a></li>
+                        <li><a href="#">Магистраль</a></li>
+                        <li><a href="#">Северные земли</a></li>
                     </ul>
                 </div>
+                <div class="content-header-section">
+                    @guest
+                        <ul class="nav-main-header">
+                            <li><a href="#"><i class="si si-user mr-5"></i>Войти</a></li>
+                            <li><a href="#"><i class="si si-key"></i>Регистрация</a></li>
+                        </ul>
+                    @endguest
+                    @auth
+                        <div class="btn-group" role="group">
+                            <button type="button" class="btn btn-rounded btn-dual-secondary" id="page-header-user-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="si si-user d-sm-none"></i>
+                                <span class="d-none d-sm-inline-block">{{ Auth::user()->nickname }}</span>
+                                <i class="fa fa-angle-down ml-5"></i>
+                            </button>
+                            <div class="dropdown-menu dropdown-menu-right min-width-200" aria-labelledby="page-header-user-dropdown">
+                                <a href="#" class="dropdown-item"><i class="si si-user mr-5"></i>Профиль</a>
+                                <div class="dropdown-divider"></div>
+                                <a href="#" class="dropdown-item">
+                                    <i class="si si-logout mr-5">Выйти</i>
+                                </a>
+                            </div>
+                        </div>
+                    @endauth
+                </div>
             </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
+        </header>
+        <main id="main-container">
+            <div class="bg-primary-darker text-body-color-light">
+                <div class="content">
+                    @yield('content')
+                </div>
+            </div>
         </main>
+        <footer id="page-footer">
+            <div class="content py-20 font-size-xs clearfix">
+
+            </div>
+        </footer>
     </div>
 </body>
+
+<!-- Codebase Core JS -->
+<script src="{{ asset('assets/js/core/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
+<script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/js/core/jquery.appear.min.js') }}"></script>
+<script src="{{ asset('assets/js/codebase.js') }}"></script>
+
 </html>
