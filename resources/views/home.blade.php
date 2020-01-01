e3433e34ca0e5002b764e4944ef8eb38f036a05f
dmitriyl99 <dmitriyl1899@gmail.com>
2020-01-01 22:06:22 +0500

    Create base home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,10 +48,37 @@
     </div>
     <div class="block block-rounded text-body-color-light mt-20 bg-primary-dark-op js-appear-enabled animated fadeIn" data-toggle="appear">
         <div class="block-header">
-            <h3 class="block-title text-body-color-light font-w700 text-center">S.T.A.L.K.E.R. Underground</h3>
+            <h3 class="block-title text-body-color-light font-w700 text-center">Локации</h3>
         </div>
         <div class="block-content bg-primary-dark">
-
+            <table class="table table-stripped table-borderless table-vcenter">
+                <tbody>
+                    @foreach($areas as $area)
+                        <tr>
+                            <td class="text-center" style="width: 65px;"><i class="si si-compass fa-2x"></i></td>
+                            <td colspan="2">
+                                <span class="font-size-h5 text-primary font-w600">{{ $area->name }}</span>
+                            </td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        @foreach($area->locations as $location)
+                            <tr>
+                                <td></td>
+                                <td class="text-center" style="width: 65px;"><i class="si si-compass fa-2x"></i></td>
+                                <td><a href="/{{ $area->slug }}/{{ $location->slug }}" class="font-size-h5 font-w600">{{ $location->name }}</a></td>
+                                <td class="d-none d-md-table-cell text-right">
+                                    <span class="font-size-sm">
+                                        от <a href="">Нулевой</a>
+                                        <br>
+                                        <em><span class="font-w600">23:08</span> | 20.12.2019</em>
+                                    </span>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
