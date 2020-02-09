0389fbafaab555a574f08f5b4f0adf92ed104fda
dmitriyl99 <dmitriyl1899@gmail.com>
2020-02-09 22:53:35 +0500

    View for create a hero

---
+++ resources/views/users/hero/create.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Создать персонажа')
+
+@section('content')
+    <div class="block block-rounded text-body-color-light mt-20 bg-primary-dark-op js-appear-enabled animated fadeIn" data-toggle="appear">
+        <div class="block-header">
+            <h3 class="block-title text-body-color-light text-center">Создание персонажа</h3>
+        </div>
+        <div class="block-content bg-primary-dark">
+
+        </div>
+    </div>
+@endsection
