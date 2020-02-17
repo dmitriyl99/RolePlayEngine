ce19ec1a43215c7c799583b9d1c6c09798a24902
dmitriyl99 <dmitriyl1899@gmail.com>
2020-02-18 01:14:37 +0500

    Add confirmation button to profile page

--- resources/views/info/profile.blade.php
+++ resources/views/info/profile.blade.php
@@ -9,6 +9,14 @@
         <div class="block-header">
             <h3 class="block-title text-body-color-light text-center">Анкета персонажа <span class="text-primary font-w700">{{ $profile->hero->getName() }}</span> <small class="text-muted">(владелец:
                     <a href="{{ route('profile.show', $profile->hero->user->id) }}">{{ $profile->hero->user->nickname }}</a>)</small></h3>
+            @if (Auth::user() && Auth::user()->hasRole('game_master'))
+                <div class="block-options">
+                    <form action="{{ route('profiles.confirm', $profile->id) }}" method="post">
+                        @csrf
+                        <button type="submit" class="btn-option text-body-color-light bg-transparent" style="border: none;"><i class="fa fa-check mr-10"></i>Принять</button>
+                    </form>
+                </div>
+            @endif
         </div>
         <div class="block-content bg-primary-dark profile-content pb-20">
             {!! $profile->content !!}
