6c6e56ef6d632f46e14b4ea0735d134c200f8adc
dmitriyl99 <dmitriyl1899@gmail.com>
2020-02-18 01:13:01 +0500

    Fix

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -3,14 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class NotificationsController extends Controller
 {
     public function markAsRead()
     {
         \Auth::user()->unreadNotifications->markAsRead();
-        Auth::user()
 
         return response(null, 200);
     }
