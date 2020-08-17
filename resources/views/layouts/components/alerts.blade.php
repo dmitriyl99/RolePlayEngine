6e8dd0e4f47f25b5fc1da0600b6ec053fb6a5d2d
dmitriyl99 <dmitriyl1899@gmail.com>
2020-08-17 16:37:46 +0500

    Alerts component

---
+++ resources/views/layouts/components/alerts.blade.php
@@ -0,0 +1,36 @@
+@if ($message = Session::get('success'))
+    <div class="alert alert-success alert-block alert-dismissible fade show">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+
+@if ($message = Session::get('error'))
+    <div class="alert alert-danger alert-block alert-dismissible fade show">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+
+@if ($message = Session::get('warning'))
+    <div class="alert alert-warning alert-block alert-dismissible fade show">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+
+@if ($message = Session::get('info'))
+    <div class="alert alert-info alert-block alert-dismissible fade show mb-0">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
