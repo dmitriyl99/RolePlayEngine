de49c8eaf2822c9c3acf7c0b485d29d47f1babf6
dmitriyl99 <dmitriyl1899@gmail.com>
2020-08-23 02:08:19 +0500

    Area Form request

---
+++ app/Http/Requests/AreaRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AreaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->hasRole('admin');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => __('Type name of area'),
+            'name.string' => __('Name of area must be a string'),
+            'name.max' => __('Name of area must be less than :max symbols')
+        ];
+    }
+}
