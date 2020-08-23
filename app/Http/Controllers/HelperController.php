06f33cbadad9c98032c61a19db634588e96d998e
dmitriyl99 <dmitriyl1899@gmail.com>
2020-08-23 18:48:47 +0500

    Store ckeditor images

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class HelperController extends Controller
@@ -12,8 +14,8 @@ class HelperController extends Controller
     public function uploadCkfinderImage(Request $request) {
         if ($request->file('upload')) {
             $image = $request->file('upload');
-            $filename = Str::random(10) . '.' . $image->extension();
-            $image->storeAs(self::UPLOAD_DIRECTORY, $filename);
+            $filename = Str::random(10) . '.' . $image->getClientOriginalExtension();
+            Storage::disk('public')->put('uploads/' . $filename, File::get($image));
             $url = asset(self::UPLOAD_DIRECTORY . $filename);
             return response()->json([ 'fileName' => $filename, 'uploaded' => true, 'url' => $url, ]);
         }
