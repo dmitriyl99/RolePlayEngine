47e2fb3f08009ef7f5d44037564a8735dea7a8b7
dmitriyl99 <dmitriyl1899@gmail.com>
2020-08-23 16:44:13 +0500

    Slug for places

--- app/Place.php
+++ app/Place.php
@@ -4,10 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Place extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasSlug;
     /**
      * @var array
     */
@@ -33,5 +35,12 @@ class Place extends Model
     public function location()
     {
         return $this->belongsTo(Location::class);
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
     }
 }
