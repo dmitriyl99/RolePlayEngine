37d47a7b089fb1f0f5fa2c4643d8f1cde7e1d182
dmitriyl99 <dmitriyl1899@gmail.com>
2020-08-25 18:59:41 +0500

    Add sidebar to layouts

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -49,6 +49,66 @@
 </head>
 <body>
 <div id="page-container" class="side-scroll page-header-modern page-header-inverse page-header-fixed main-content-boxed">
+    <nav id="sidebar">
+        <!-- Sidebar Scroll Container -->
+        <div id="sidebar-scroll">
+            <!-- Sidebar Content -->
+            <div class="sidebar-content">
+                <!-- Side Header -->
+                <div class="content-header content-header-fullrow px-15">
+                    <!-- Mini Mode -->
+                    <div class="content-header-section sidebar-mini-visible-b">
+                        <!-- Logo -->
+                        <span class="content-header-item font-w700 font-size-xl float-left animated fadeIn">
+                                    <span class="text-dual-primary-dark">c</span><span class="text-primary">b</span>
+                                </span>
+                        <!-- END Logo -->
+                    </div>
+                    <!-- END Mini Mode -->
+
+                    <!-- Normal Mode -->
+                    <div class="content-header-section text-center align-parent sidebar-mini-hidden">
+                        <!-- Close Sidebar, Visible only on mobile screens -->
+                        <!-- Layout API, functionality initialized in Codebase() -> uiApiLayout() -->
+                        <button type="button" class="btn btn-circle btn-dual-secondary d-lg-none align-v-r" data-toggle="layout" data-action="sidebar_close">
+                            <i class="fa fa-times text-danger"></i>
+                        </button>
+                        <!-- END Close Sidebar -->
+
+                        <!-- Logo -->
+                        <div class="content-header-item">
+                            <a href="{{ route('home') }}" class="">
+                                <img src="{{ asset('assets/img/logo.png') }}" alt="" style="height: 40px">
+                            </a>
+                        </div>
+                        <!-- END Logo -->
+                    </div>
+                    <!-- END Normal Mode -->
+                </div>
+                <!-- END Side Header -->
+
+                <!-- Side Navigation -->
+                <div class="content-side content-side-full">
+                    <ul class="nav-main text-body-color-light">
+                        @guest
+                            <li class="nav-main-heading">
+                                Авторизация
+                            </li>
+                            <li><a href="{{ route('login') }}"><i class="si si-user mr-5"></i>Войти</a></li>
+                            <li><a href="{{ route('register') }}"><i class="si si-key"></i>Регистрация</a></li>
+                        @endguest
+                        <li class="nav-main-heading">Локации</li>
+                        @foreach($areas as $area)
+                            <li><a href="{{ route('area', $area->slug) }}"><i class="si si-compass"></i>{{ $area->name }}</a></li>
+                        @endforeach
+                    </ul>
+                </div>
+                <!-- END Side Navigation -->
+            </div>
+            <!-- Sidebar Content -->
+        </div>
+        <!-- END Sidebar Scroll Container -->
+    </nav>
     <header id="page-header">
         <div class="content-header">
             <div class="content-header-section">
